57aa656ed70ea8dee71162c5a75dad7e64332c46
mounkambou-iut-cse-4301 <37999654+mounkambou-iut-cse-4301@users.noreply.github.com>
2019-04-06 05:28:27 +0600

    sign up

---
+++ sign_up_patient.js
@@ -0,0 +1,38 @@
+var boton2 = document.getElementById("btn_field5");
+boton2.addEventListener("click", hide2);
+function hide2(e) {
+    e.preventDefault();
+    var form1 = document.getElementById("field4");
+    var form2 = document.getElementById("field5");
+    var form3 = document.getElementById("field6");
+    form2.style.display = "block";
+    form1.style.display = "none";
+    form3.style.display = "none";
+
+}
+
+var boton3 = document.getElementById("btn_field6");
+boton3.addEventListener("click", hide3);
+function hide3(e) {
+    e.preventDefault();
+    var form1 = document.getElementById("field4");
+    var form2 = document.getElementById("field5");
+    var form3 = document.getElementById("field6");
+    form3.style.display = "block";
+    form1.style.display = "none";
+    form2.style.display = "none";
+
+}
+
+var boton1 = document.getElementById("btn_field4");
+boton1.addEventListener("click", hide1);
+function hide1(e) {
+    e.preventDefault();
+    var form1 = document.getElementById("field4");
+    var form2 = document.getElementById("field5");
+    var form3 = document.getElementById("field6");
+    form1.style.display = "block";
+    form2.style.display = "none";
+    form3.style.display = "none";
+
+}
